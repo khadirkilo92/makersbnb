861d7d7fcae637869cdddfc0a319075e49f7dc6b
Daniel Taplin <98479830+dantaplin992@users.noreply.github.com>
2022-04-07 11:50:33 +0100

    added number of nights

--- spec/features/date_picker_spec.rb
+++ spec/features/date_picker_spec.rb
@@ -9,4 +9,32 @@ feature 'calendar on the preview page' do
     expect(current_path.split('/')[1]).to eq 'booking_preview'
     expect(page).to have_field 'From Date'
   end 
+
+  scenario 'it has a text box for Number of Nights on the page' do
+    test_login
+    add_test_space
+    visit '/spaces'
+    first('.item').click_button 'Book Now'
+    expect(current_path.split('/')[1]).to eq 'booking_preview'
+    expect(page).to have_field 'Number of Nights'
+  end
+
+  scenario 'calculating the default price' do
+    test_login
+    add_test_space
+    visit '/spaces'
+    first('.item').click_button 'Book Now'
+    expect(current_path.split('/')[1]).to eq 'booking_preview'
+    expect(first('.totalprice')).to have_content "300"
+  end
+
+  scenario 'filling in Number of Nights and calculating the price' do
+    test_login
+    add_test_space
+    visit '/spaces'
+    first('.item').click_button 'Book Now'
+    expect(current_path.split('/')[1]).to eq 'booking_preview'
+    fill_in "Number of Nights", with: '3'
+    expect(first('.totalprice')).to have_content "900"
+  end
 end 
