823e44daeeee29c4c5caf70eb8acfb20ff409804
Khushnood Asif <khushnood99@gmail.com>
2022-04-07 10:37:24 +0100

    merge conflict issue

--- spec/features/confirm_booking_spec.rb
+++
@@ -1,8 +0,0 @@
-feature 'shows the preview page' do
-  scenario 'shows the preview confirmation page' do
-    result = DatabaseConnection.query("INSERT INTO spaces (name, description, price_per_night) VALUES($1, $2, $3);", ['1 bed flat', 'abc', "100"])
-    visit '/spaces'
-    click_button 'Book now'
-    expect(page).to have_content 'Booking Preview'
-    end 
-end 
--- spec/features/viewing_pages_spec.rb
+++
@@ -1,9 +0,0 @@
-feature 'home page' do 
-  scenario 'view the home page' do
-    visit ('/')
-    expect(page).to have_content "Welcome to Makersbnb"
-    
-    click_button "Log in here"
-    expect(current_path).to eq '/log_in'
-  end
-end
